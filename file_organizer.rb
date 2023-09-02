a6341f53f7dbf8b7bbd74dbe859da09dd919f93e
Omar Odeh <omar.odeh95@hotmail.com>
2023-09-02 18:04:39 +0300

    add running logic in a method

--- file_organizer.rb
+++ file_organizer.rb
@@ -6,11 +6,35 @@ VALID_FILE_NUMBER_RANGE = (0..100).freeze
 VALID_FILE_EXTENSIONS = ['txt'].freeze
 VALID_LANGUAGE_PATTERNS = [/^[a-zA-Z]+$/].freeze
 IGNORED_ENTRIES = ['.', '..'].freeze
+PARSED_FILE_ATTRIBUTES = ['language', 'number', 'extension']
+
+
+def initialize
+  @parsed_files = {}
+end
+
+def add_to_parsed_files(file)
+    name, extension = file.split('.')
+    language, number = name.split('-')
+    number = number.to_i
+    @parsed_files[file] = {}
+    PARSED_FILE_ATTRIBUTES.each do |attribute|
+      raise "You need to define #{attribute} in parse_file_name method first!" if eval(attribute).nil?
+      @parsed_files[file][attribute]   = eval(attribute)
+    end
+end
 
 def parse_file_name(file)
-  name, extension = file.split('.')
-  language, number = name.split('-')
-  return language, number.to_i, extension
+  if @parsed_files[file].nil?
+    add_to_parsed_files(file)
+  end
+
+  returned_values = []
+  PARSED_FILE_ATTRIBUTES.each do |attribute|
+    returned_values << @parsed_files[file][attribute]
+  end
+
+  return returned_values
 end
 
 def validate_file(file)
@@ -24,17 +48,16 @@ def validate_file(file)
   true
 end
 
-# Check if the files folder exist
-raise "Files folder doesn't exist" unless File.directory?(FILES_FOLDER_PATH)
-
-# Start organizing files
-Dir.entries(FILES_FOLDER_PATH)
-   .reject { |entry| IGNORED_ENTRIES.include? entry }
-   .each do |file|
-  begin
-    validate_file(file)
-  rescue => e
-    puts "Warning: #{e} for #{file}"
-    next
+def run
+  # Start organizing files
+  Dir.entries(FILES_FOLDER_PATH)
+     .reject { |entry| IGNORED_ENTRIES.include? entry }
+     .each do |file|
+    begin
+      validate_file(file)
+    rescue => e
+      puts "Warning: #{e} for #{file}"
+      next
+    end
   end
 end
